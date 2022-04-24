0be31d3669d7f45682ca44faec03017c042b4300
Tony Du <cheeham51@hotmail.com>
2022-04-24 17:43:41 +1000

    Add stack test to test failure

--- test/pipeline_lab.test.ts
+++ test/pipeline_lab.test.ts
@@ -1,17 +1,17 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as PipelineLab from '../lib/pipeline_lab-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as PipelineLab from '../lib/pipeline_lab-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/pipeline_lab-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
+test('Test Pipeline Lab Stack', () => {
+  const app = new cdk.App();
 // //     // WHEN
-//   const stack = new PipelineLab.PipelineLabStack(app, 'MyTestStack');
+  const stack = new PipelineLab.PipelineLabStack(app, 'MyTestStack');
 // //     // THEN
-//   const template = Template.fromStack(stack);
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties('AWS::SQS::Queue', {
+    VisibilityTimeout: 300
+  });
 });
