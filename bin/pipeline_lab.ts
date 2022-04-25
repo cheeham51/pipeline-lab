c55076c308dcdd4b39c174352f7cb2bf0766db3e
Tony Du <cheeham51@hotmail.com>
2022-04-25 18:44:38 +1000

    remove billing stack

--- bin/pipeline_lab.ts
+++ bin/pipeline_lab.ts
@@ -2,17 +2,17 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PipelineLabStack } from '../lib/pipeline_lab-stack';
-import { BillingStack } from '../lib/billing-stack';
+// import { BillingStack } from '../lib/billing-stack';
 import { ServiceStack } from '../lib/service-stack';
 
 const app = new cdk.App();
 
 const pipelineLabStack = new PipelineLabStack(app, 'PipelineLabStack', {});
 
-new BillingStack(app, 'BillingStack', {
-  budgetAmount: 5,
-  emailAddress: 'cheeham51@hotmail.com',
-});
+// new BillingStack(app, 'BillingStack', {
+//   budgetAmount: 5,
+//   emailAddress: 'cheeham51@hotmail.com',
+// });
 
 const serviceStack = new ServiceStack(app, 'ServiceStack');
 
