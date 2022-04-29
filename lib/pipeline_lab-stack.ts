f37483e68e643bc0781f2384ccb22b7eae031f9d
Tony Du <cheeham51@hotmail.com>
2022-04-29 17:10:56 +1000

    Add notifcation on integration test failure

--- lib/pipeline_lab-stack.ts
+++ lib/pipeline_lab-stack.ts
@@ -3,18 +3,32 @@ import { Construct } from 'constructs';
 import { Artifact, IStage, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
 import { CodeBuildAction, CloudFormationCreateUpdateStackAction, GitHubSourceAction, CodeBuildActionType } from 'aws-cdk-lib/aws-codepipeline-actions';
 import { BuildEnvironmentVariableType, BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
+import { Topic } from 'aws-cdk-lib/aws-sns';
+
 
 import { ServiceStack } from './service-stack';
 import { BillingStack } from './billing-stack';
+import { SnsTopic } from 'aws-cdk-lib/aws-events-targets';
+import { EventField, RuleTargetInput } from 'aws-cdk-lib/aws-events';
+import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 
 export class PipelineLabStack extends Stack {
   private readonly pipeline: Pipeline;
   private readonly cdkBuildOutput: Artifact;
   private readonly serviceBuildOutput: Artifact;
   private readonly serviceSourceOutput: Artifact;
+  private readonly pipelineNotificationsTopic: Topic;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    this.pipelineNotificationsTopic = new Topic(this, 'PipelineNotificationsTopic', {
+      topicName: 'PipelineNotifications'
+    })
+
+    this.pipelineNotificationsTopic.addSubscription(
+      new EmailSubscription('cheeham51@hotmail.com')
+    )
 
     this.pipeline = new Pipeline(this, 'MyPipeline', {
       crossAccountKeys: false,
@@ -122,25 +136,39 @@ export class PipelineLabStack extends Stack {
   }
 
   public addServiceIntegrationTestToStage(stage: IStage, serviceEndpoint: string) {
-    stage.addAction(
-      new CodeBuildAction({
-        actionName: "IntegrationTests",
-        input: this.serviceSourceOutput,
-        project: new PipelineProject(this, 'ServiceIntegrationTestsProject', {
-          environment: {
-            buildImage: LinuxBuildImage.STANDARD_5_0
-          },
-          buildSpec: BuildSpec.fromSourceFilename('build_specs/integ_test_build_specs.yml')
-        }),
-        environmentVariables: {
-          SERVICE_ENDPOINT: {
-            value: serviceEndpoint,
-            type: BuildEnvironmentVariableType.PLAINTEXT
+    const integrationTestAction = new CodeBuildAction({
+      actionName: "IntegrationTests",
+      input: this.serviceSourceOutput,
+      project: new PipelineProject(this, 'ServiceIntegrationTestsProject', {
+        environment: {
+          buildImage: LinuxBuildImage.STANDARD_5_0
+        },
+        buildSpec: BuildSpec.fromSourceFilename('build_specs/integ_test_build_specs.yml')
+      }),
+      environmentVariables: {
+        SERVICE_ENDPOINT: {
+          value: serviceEndpoint,
+          type: BuildEnvironmentVariableType.PLAINTEXT
+        }
+      },
+      type: CodeBuildActionType.TEST,
+      runOrder: 2
+    })
+    stage.addAction(integrationTestAction)
+    integrationTestAction.onStateChange(
+      'IntegrationTestFailed',
+      new SnsTopic(this.pipelineNotificationsTopic, {
+        message: RuleTargetInput.fromText(`Integration Test Failed. See details here: ${EventField.fromPath('$.detail.execution-result.external-execution-url')}`)
+      }),
+      {
+        ruleName: 'IntegrationTestFailed',
+        eventPattern: {
+          detail: {
+            state: ['FAILED']
           }
         },
-        type: CodeBuildActionType.TEST,
-        runOrder: 2
-      })
+        description: 'Integration test has failed'
+      }
     )
   }
 }
